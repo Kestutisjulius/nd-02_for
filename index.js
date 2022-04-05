afaa5697f533d6a6a2c0af4e95a0620adb0633b2
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-04-05 15:33:19 +0300

    Nd-02-ciklas

---
+++ index.js
@@ -0,0 +1,5 @@
+const nd2 = require('./js/01-labas.js');
+
+/*1. Labas FOR  */
+console.log('nd2 - LABAS--------2022.04.01');
+console.log('--1.)' + nd2.labas());
---
+++ js/01-labas.js
@@ -0,0 +1,7 @@
+const labas = function (){
+    for (var i = 1; i <= 5; i++) {
+        console.log(`${i}). Labas`);
+    }
+    return 'done "Labas"';
+}
+module.exports.labas = labas;
