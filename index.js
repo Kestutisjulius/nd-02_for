c533a3ee023c738fb251e8a2aff8cef3a6f4b4d8
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-04-05 15:56:09 +0300

    ND-02_04_for-49-53

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const nd2_1 = require('./js/01-labas.js');
 const nd2_2 = require('./js/02-one-line.js');
 const nd2_3 = require('./js/03-step10.js');
+const nd2_4 = require('./js/04-for-49-53.js');
 
 /*1. Labas FOR  */
 // console.log('nd2 - LABAS--------2022.04.01');
@@ -8,6 +9,9 @@ const nd2_3 = require('./js/03-step10.js');
 /*2. One Line  */
 // console.log('nd2 - To One Line --------2022.04.01');
 // console.log('--2.) ' + nd2_2.oL());
-/*2. One Line  */
-console.log('nd2 - step 10  --------2022.04.01');
-console.log('--3.) ' + nd2_3.step());
+/*3. Step 10  */
+// console.log('nd2 - step 10  --------2022.04.01');
+// console.log('--3.) ' + nd2_3.step());
+/*4. for-49-53  */
+console.log('nd2 - for-49-53  --------2022.04.01');
+console.log('--4.) ' + nd2_4.four());
---
+++ js/04-for-49-53.js
@@ -0,0 +1,8 @@
+const four = function (){
+    
+    for (var i = 49; i <= 53; i++){
+        console.log(i);
+    }
+    return 'Done.';
+}
+module.exports.four = four;
